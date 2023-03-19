c7fd3400f7503c9a697bf095b8d2c090ab21c092
opnuub <cody.liush@gmail.com>
2023-03-19 18:21:54 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c492dbaa8e669c0aa559d3e94073fedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b466ce24273bbb0b4d34",
+    "url": "/asrjcwebsite/static/css/main.4c70dc30.chunk.css"
+  },
+  {
+    "revision": "b466ce24273bbb0b4d34",
+    "url": "/asrjcwebsite/static/js/main.b466ce24.chunk.js"
+  },
+  {
+    "revision": "62cac4abb9d6098befd4",
+    "url": "/asrjcwebsite/static/js/1.62cac4ab.chunk.js"
+  },
+  {
+    "revision": "557a249d253a3eea4759",
+    "url": "/asrjcwebsite/static/js/2.557a249d.chunk.js"
+  },
+  {
+    "revision": "99765710438d7b0b0be1",
+    "url": "/asrjcwebsite/static/js/runtime~main.99765710.js"
+  },
+  {
+    "revision": "67e506d98110213610ea6a4612b4e143",
+    "url": "/asrjcwebsite/index.html"
+  }
+];
